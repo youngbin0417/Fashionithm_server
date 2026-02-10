0a4c3d3494b17c51afd3c0ce4b98dabd1aa4a3f9
Youngbin Ko <66235935+youngbin0417@users.noreply.github.com>
2026-02-11 03:11:49 +0900

    set up

---
+++ src/main/java/com/example/fashionithm/FashionithmApplication.java
@@ -0,0 +1,13 @@
+package com.example.fashionithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionithmApplication.class, args);
+    }
+
+}
